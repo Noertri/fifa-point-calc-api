0ecc88cfdc3e24a5ac4b7033b5b68a185a393814
Noertri <trinuruns@gmail.com>
2023-07-27 10:08:02 +0700

    memodifikasi kode run.cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-CD /D "D:\Python\fifa-point-calculator-api" & "D:\Python\venvs\flask-env\Scripts\python.exe" "index.py"
+CD /D "D:\Python\fifa-point-calculator-v2" & "D:\Python\venvs\flask-env\Scripts\python.exe" "index.py"
