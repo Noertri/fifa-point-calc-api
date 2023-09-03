6469fda5332006abb7d7e604dcc0d5d5b5d667e5
Noertri <trinuruns@gmail.com>
2023-09-03 21:09:52 +0700

    modifikasi gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import multiprocessing as mp
 
 
-bind = ["0.0.0.0:5000"]
+bind = ["unix:fifa-point-calc-api.sock"]
 worker = mp.cpu_count()*2+1
 wsgi_app = "wsgi:app"
-accesslog = "/var/log/gunicorn/api_access.log"
-errorlog = "/var/log/gunicorn/api_error.log"
+# accesslog = "/var/log/gunicorn/api_access.log"
+# errorlog = "/var/log/gunicorn/api_error.log"
