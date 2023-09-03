60eca2bd49829a06cddff2b730220a16e336c05b
Noertri <trinuruns@gmail.com>
2023-09-03 20:30:30 +0700

    tes dan debug

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 
 
 bind = ["0.0.0.0:5000"]
-worker = mp.cpu_count()*2
-wsgi_app = "index:app"
+worker = mp.cpu_count()*2+1
+wsgi_app = "wsgi:app"
 accesslog = "/var/log/gunicorn/api_access.log"
 errorlog = "/var/log/gunicorn/api_error.log"
