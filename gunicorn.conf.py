47e546dc5dce673ac7f29113dacc3c3c3dbf619a
Noertri <trinuruns@gmail.com>
2023-09-23 11:54:27 +0700

    tes hook

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,4 +5,4 @@ bind = ["unix:fifa-point-calc-api.sock"]
 worker = mp.cpu_count()*2+1
 wsgi_app = "wsgi:app"
 accesslog = "/var/log/gunicorn/api_access.log"
-# errorlog = "/var/log/gunicorn/api_error.log"
+errorlog = "/var/log/gunicorn/api_error.log"
